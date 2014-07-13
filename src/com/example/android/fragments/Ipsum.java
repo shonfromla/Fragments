044ad10c022f4b3237c44dc1f0828aa29249c29c
GitHub1505 <shondramccray@yahoo.com>
2014-07-13 18:06:54 -0500

    Fragment Basics

---
+++ src/com/example/android/fragments/Ipsum.java
@@ -0,0 +1,14 @@
+package com.example.android.fragments;
+
+public class Ipsum {
+
+    static String[] Headlines = {
+        "Dr. Bittinger Receives Award",
+        "Dr. Bittinger Guest Lecturer in London"
+    };
+
+    static String[] Articles = {
+        "Dr. Bittinger Receives Award\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mattis, nunc vel dignissim posuere, velit nunc mollis magna, quis lacinia sapien justo vel odio. Vestibulum molestie vestibulum est vel vestibulum. Aliquam molestie ullamcorper lorem non eleifend. Nam pretium luctus diam quis vestibulum. Nulla est velit, consequat at nibh vitae, luctus iaculis felis. Vivamus felis dui, laoreet non aliquam et, fringilla vitae ipsum.",
+        "Guest Lecturer: Dr. Bittinger\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mattis, nunc vel dignissim posuere, velit nunc mollis magna, quis lacinia sapien justo vel odio. Vestibulum molestie vestibulum est vel vestibulum. Aliquam molestie ullamcorper lorem non eleifend. Nam pretium luctus diam quis vestibulum. Nulla est velit, consequat at nibh vitae, luctus iaculis felis. Vivamus felis dui, laoreet non aliquam et, fringilla vitae ipsum."
+    };
+}
